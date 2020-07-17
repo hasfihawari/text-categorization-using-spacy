1862eac738d3b685999ca93a4a602efd7f1b25db
Hasfi Hawari <hasfi@jkt1.ebdesk.com>
2020-07-17 11:51:12 +0700

    add file training and predict

---
+++ predict.py
@@ -0,0 +1,24 @@
+import os
+import spacy
+import operator
+
+path_this = os.path.dirname(os.path.join(__file__))
+path_model = os.path.abspath(os.path.join(path_this, 'model'))
+
+def load_model():
+    nlp = spacy.load(path_model)
+    return nlp
+
+def predict(model, text):
+    result = model(text)
+    result = result.cats
+    result = max(result.items(), key=operator.itemgetter(1))[0] 
+    
+    final_result = {'text': text, 'predict':result}
+    return final_result
+
+if __name__ == '__main__':
+    model = load_model()
+    text = 'selamat anda mendapatkan hadiah uang tunai 100 juta'
+    
+    print(predict(model, text))
---
+++ training.py
@@ -0,0 +1,54 @@
+import os, sys
+import spacy
+import pandas as pd
+from spacy.util import minibatch
+
+path_this = os.path.dirname(os.path.join(__file__))
+path_dataset = os.path.abspath(os.path.join(path_this, 'dataset'))
+path_model = os.path.abspath(os.path.join(path_this, 'model'))
+
+def load_data():
+    data = pd.read_csv(os.path.join(path_dataset, 'dataset.csv'))
+    
+    data_train = data['Teks'].values
+    
+    train_label = [
+    {
+        'cats' : {
+        'normal': label == 0, 
+        'fraud': label == 1, 
+        'promo': label == 2
+        }
+    } 
+    for label in data['label']]
+    
+    train_data = list(zip(data_train, train_label))
+    
+    return train_data
+
+def training(train_data):
+    nlp = spacy.blank('id')
+    
+    textcat = nlp.create_pipe('textcat')
+    
+    nlp.add_pipe(textcat)
+    
+    textcat.add_label('normal')
+    textcat.add_label('fraud')
+    textcat.add_label('promo')
+    
+    optimizer = nlp.begin_training()
+
+    losses = {}
+    for epoch in range(10):
+        batches = minibatch(train_data, size=5)
+        for batch in batches:
+            texts, labels = zip(*batch)
+            nlp.update(texts, labels, sgd=optimizer, losses=losses)
+        print('epoch : {}'.format(epoch))
+        
+    nlp.to_disk(path_model)
+    
+if __name__ == '__main__':
+    data = load_data()
+    training(data)
